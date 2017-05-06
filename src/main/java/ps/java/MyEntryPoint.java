c9a4305b1a300d133d704493e49e2bfede184e39
Petr Sládek <petr.sladek@i.cz>
2017-05-06 11:52:54 +0200

    Brainfuck client interpreter initialization

---
+++ src/main/java/ps/java/MyEntryPoint.java
@@ -0,0 +1,23 @@
+`package org.test;
+
+import com.google.gwt.core.client.EntryPoint;
+//import com.google.gwt.event.dom.client.ClickEvent;
+//import com.google.gwt.event.dom.client.ClickHandler;
+//import com.google.gwt.user.client.ui.Button;
+//import com.google.gwt.user.client.ui.RootPanel;
+
+public class MyEntryPoint implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        // Create a button widget
+//        Button button = new Button();
+//        button.setText("Click me!");
+//        button.addClickHandler(new ClickHandler() {
+//            @Override
+//            public void onClick(ClickEvent event) {
+//                button.setText("Hello, world!");
+//            }
+//        });
+//        RootPanel.get().add(button);
+    }
+}
